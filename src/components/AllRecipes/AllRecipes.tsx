b7a9e806cea07e6f6743d13d17178bacb8ce0e0a
aandrv260 <angel42505@outlook.com>
2022-07-17 15:39:53 +0300

    Rebuit the design
    Header changed. Added CSS base variables and hover effects on the main content area.

--- src/components/AllRecipes/AllRecipes.tsx
+++ src/components/AllRecipes/AllRecipes.tsx
@@ -12,27 +12,53 @@ import { Recipes, RecipesJSON } from '../../types/RecipeAPI';
 // Styles
 import scss from './AllRecipes.module.scss';
 import Recipe from '../Recipe/Recipe';
+import LoaderSpinner from '../LoadingSpinner/LoadingSpinner';
 
 const AllRecipes = () => {
   const [recipes, setRecipes] = useState<Recipes>([]);
+  const [recipesLoading, setRecipesLoading] = useState<boolean>(false);
   console.log(recipes);
 
   useEffect(() => {
     const setData = async () => {
       try {
+        setRecipesLoading(true);
+
         const { data } = await fetchRecipeData<RecipesJSON>('/recipes');
         setRecipes(data.recipes);
+
+        setRecipesLoading(false);
       } catch (err: unknown) {
         handleAsyncAwaitErrors(err);
+        setRecipesLoading(false);
       }
     };
 
     setData();
   }, []);
 
+  const ShowRecipes = () => {
+    return recipesLoading ? (
+      <LoaderSpinner />
+    ) : (
+      <>
+        {recipes.map(recipe => (
+          <Recipe recipe={recipe} key={generateID()} />
+        ))}
+      </>
+    );
+  };
+
   return (
-    <div className={addCSSClassName(scss, 'all-recipes', 'grid', 'grid--4-cols')}>
-      {recipes.map(recipe => (
+    <div
+      className={addCSSClassName(
+        scss,
+        'all-recipes',
+        'grid',
+        `grid--${recipesLoading ? '1-col' : '4-cols'}`
+      )}
+    >
+      {/* {recipes.map(recipe => (
         <Recipe recipe={recipe} key={generateID()} />
       ))}
 
@@ -42,7 +68,10 @@ const AllRecipes = () => {
 
       {recipes.map(recipe => (
         <Recipe recipe={recipe} key={generateID()} />
-      ))}
+      ))} */}
+      <ShowRecipes />
+      <ShowRecipes />
+      <ShowRecipes />
     </div>
   );
 };
--- src/components/LoadingSpinner/LoadingSpinner.tsx
+++ src/components/LoadingSpinner/LoadingSpinner.tsx
@@ -1,6 +1,6 @@
 import { addCSSClassName } from '../../assets/ts/generalFunctions';
-import scss from './LoaderSpinner.module.scss';
+import scss from './LoadingSpinner.module.scss';
 
-const LoaderSpinner = () => <div className={addCSSClassName(scss, 'spinner')}></div>;
+const LoaderSpinner = () => <div className={addCSSClassName(scss, 'spinner', 'text-center')}></div>;
 
 export default LoaderSpinner;
